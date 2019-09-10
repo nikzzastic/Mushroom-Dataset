000b3a3932ab53f9a93f3747b15334696a715851
Nikhil <33477525+nikzzastic@users.noreply.github.com>
2019-09-10 19:35:33 +0530

    Update mushroom.py

--- mushroom.py
+++ mushroom.py
@@ -1,3 +1,8 @@
+  
+# I wrote this code in Jupyter Notebook but there's some issue with github showing .ipynb files(thread already opened)
+# So, I am changing the extension from .ipynb to .py so that if anyone wants, he/she can see the code easily
+# For each new line, that code is actually written in new cell of jupyter notebook
+
 import pandas as pd
 import numpy as np
 import seaborn as sns
